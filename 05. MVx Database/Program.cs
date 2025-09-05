6876056a4012db5f478194808c63dd1d029e46b1
IJuniorStudent <jolan07@list.ru>
2025-09-05 19:07:03 +0300

    MVx database: preview service pack 1

--- 05. MVx Database/Program.cs
+++ 05. MVx Database/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System.Data;
 using System.Reflection;
-using System.Security.Cryptography;
-using System.Text;
 using Microsoft.Data.Sqlite;
 
 namespace MvxDatabase;
@@ -10,351 +8,146 @@ static class Program
 {
     private static void Main(string[] args)
     {
-        var logger = new ConsoleLogger();
-        var hasher = new HashCalculator(SHA256.Create());
-        
-        var userDataValidator = new UserIdentifierValidator();
-        var userDataGenerator = new UserIdentifierGenerator();
-        var userDataRequester = new ConsoleUserDataRequester(userDataGenerator, userDataValidator);
-        
-        IIdentifier userIdentifier = userDataRequester.Get();
-        var extractor = new UserIdentifierExtractor(userIdentifier);
-        
         string appRootPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
         string databaseName = "db.sqlite";
-        using var link = new Sqlite3Link($"{appRootPath}\\{databaseName}");
         
-        if (link.TryConnect() == false)
-        {
-            logger.DisplayMessage($"Failed to connect database. Error: {link.ErrorMessage}");
-            return;
-        }
+        var selector = new SqliteSelectModel($"{appRootPath}\\{databaseName}");
+        var presenter = new ConsolePresenter(selector);
         
-        var selector = new SqliteIdentifierDataSelector(extractor, hasher);
-        DataTable selected = selector.Select(link);
-        
-        var checker = new VoteParticipantChecker(logger);
-        checker.DisplayCheckResult(selected);
+        presenter.DisplayCitizenInfo();
     }
 }
 
-public interface IDataTableChecker
+public class Passport
 {
-    public void DisplayCheckResult(DataTable table);
-}
-
-public class VoteParticipantChecker : IDataTableChecker
-{
-    private ILogger _logger;
+    private const int MinIdLength = 10;
+    private const string WhiteSpace = " ";
+    private const string EmptyString = "";
     
-    public VoteParticipantChecker(ILogger logger)
+    public Passport(string id)
     {
-        _logger = logger;
+        string passportId = id.Trim();
+        
+        if (passportId == EmptyString)
+            throw new ArgumentException("Empty id");
+        
+        Id = passportId.Replace(WhiteSpace, EmptyString);
+        
+        if (Id.Length < MinIdLength)
+            throw new ArgumentException($"Passport id must contain at least {MinIdLength} characters");
     }
     
-    public void DisplayCheckResult(DataTable table)
-    {
-        if (table.Rows.Count == 0)
-        {
-            _logger.DisplayMessage("По данному паспорту данные не найдены");
-            return;
-        }
-        
-        bool hasAccess = Convert.ToBoolean(table.Rows[0].ItemArray[1]);
-        string accessResult = hasAccess ? "ПРЕДОСТАВЛЕН" : "НЕ ПРЕДОСТАВЛЯЛСЯ";
-        
-        _logger.DisplayMessage($"По данному паспорту доступ к бюллетеню на дистанционном электронном голосовании {accessResult}");
-    }
+    public string Id { get; }
 }
 
-public interface ILogger
+public class Citizen
+{
+    public Citizen(Passport passport, bool hasVoteAccess)
+    {
+        Passport = passport;
+        HasVoteAccess = hasVoteAccess;
+    }
+    
+    public Passport Passport { get; }
+    public bool HasVoteAccess { get; }
+}
+
+public interface IView
 {
     public void DisplayMessage(string message);
+    public string RequestPassportId();
 }
 
-public class ConsoleLogger : ILogger
+public class ConsoleView : IView
 {
     public void DisplayMessage(string message)
     {
-        Console.WriteLine(message);
-    }
-}
-
-public interface IQueryTransformer
-{
-    public const int DefaultLimit = 0;
-    public string GenerateQuery(string table, string condition, int limit = DefaultLimit);
-}
-
-public class SqliteQueryTransformer : IQueryTransformer
-{
-    public string GenerateQuery(string table, string condition, int limit)
-    {
-        string limitString = limit == IQueryTransformer.DefaultLimit ? "" : $" limit {limit}";
-        return $"SELECT * FROM {table} WHERE {condition}{limitString};";
-    }
-}
-
-public interface IDataSelector
-{
-    public DataTable Select(DatabaseLink link);
-}
-
-public struct SelectionPrefs
-{
-    public string Table { get; }
-    public string Condition { get; }
-    public int Limit { get; }
-    
-    public SelectionPrefs(string table, string condition, int limit)
-    {
-        Table = table;
-        Condition = condition;
-        Limit = limit;
-    }
-}
-
-public abstract class IdentifierDataSelector : IDataSelector
-{
-    private IDataExtractor _extractor;
-    private IHashCalculator _hasher;
-    private IQueryTransformer _transformer;
-    
-    protected IdentifierDataSelector(IDataExtractor dataExtractor, IHashCalculator hasher, IQueryTransformer transformer)
-    {
-        _extractor = dataExtractor;
-        _hasher = hasher;
-        _transformer = transformer;
+        Console.WriteLine();
     }
     
-    public DataTable Select(DatabaseLink link)
-    {
-        SelectionPrefs prefs = InitPrefs();
-        string query = _transformer.GenerateQuery(prefs.Table, prefs.Condition, prefs.Limit);
-        
-        return link.Select(query);
-    }
-    
-    protected string CalculateDataHash()
-    {
-        return _hasher.Calculate(_extractor.Data);
-    }
-    
-    protected abstract SelectionPrefs InitPrefs();
-}
-
-public class SqliteIdentifierDataSelector : IdentifierDataSelector
-{
-    public SqliteIdentifierDataSelector(IDataExtractor dataExtractor, IHashCalculator hasher) : base(dataExtractor, hasher, new SqliteQueryTransformer()) { }
-    
-    protected override SelectionPrefs InitPrefs()
-    {
-        return new SelectionPrefs("passports", $"num = '{CalculateDataHash()}'", 1);
-    }
-}
-
-public abstract class DatabaseLink : IDisposable
-{
-    private readonly string _connectionAddress;
-    
-    protected DatabaseLink(string address)
-    {
-        _connectionAddress = address;
-        ErrorMessage = "";
-    }
-    
-    public string ErrorMessage { get; protected set; }
-    
-    public bool TryConnect()
-    {
-        return TryInitializeConnection(_connectionAddress);
-    }
-    
-    public void Dispose()
-    {
-        Disconnect();
-    }
-    
-    public abstract DataTable Select(string query);
-    
-    protected abstract bool TryInitializeConnection(string address);
-    protected abstract void Disconnect();
-}
-
-public class Sqlite3Link : DatabaseLink
-{
-    private SqliteConnection? _connection = null;
-    
-    public Sqlite3Link(string address) : base(address) { }
-    
-    public override DataTable Select(string query)
-    {
-        throw new NotImplementedException();
-    }
-
-    protected override bool TryInitializeConnection(string address)
-    {
-        if (File.Exists(address) == false)
-        {
-            ErrorMessage = $"Database file \"{address}\" does not exist";
-            return false;
-        }
-        
-        try
-        {
-            _connection = new SqliteConnection($"Data Source={address}");
-            _connection.Open();
-            return true;
-        }
-        catch (SqliteException e)
-        {
-            ErrorMessage = e.Message;
-            return false;
-        }
-    }
-    
-    protected override void Disconnect()
-    {
-        _connection?.Close();
-        _connection = null;
-    }
-}
-
-public interface IUserDataRequester
-{
-    public IIdentifier Get();
-}
-
-public abstract class UserDataRequester : IUserDataRequester
-{
-    private readonly IIdentifierGenerator _generator;
-    private readonly IIdentifierValidator _validator;
-    
-    protected UserDataRequester(IIdentifierGenerator generator, IIdentifierValidator validator)
-    {
-        _generator = generator;
-        _validator = validator;
-    }
-    
-    public IIdentifier Get()
-    {
-        string userData = RetrieveUserData();
-        return _generator.Create(userData, _validator);
-    }
-
-    protected abstract string RetrieveUserData();
-}
-
-public class ConsoleUserDataRequester : UserDataRequester
-{
-    public ConsoleUserDataRequester(IIdentifierGenerator generator, IIdentifierValidator validator) : base(generator, validator) {}
-    
-    protected override string RetrieveUserData()
+    public string RequestPassportId()
     {
         return Console.ReadLine()!;
     }
 }
 
-public interface IHashCalculator
+public interface IPresenter
 {
-    public string Calculate(string input);
+    public void DisplayCitizenInfo();
 }
 
-public class HashCalculator : IHashCalculator
+public class ConsolePresenter : IPresenter
 {
-    private HashAlgorithm _algorithm;
+    private readonly ProcessDataModel _processor;
+    private readonly IView _view;
     
-    public HashCalculator(HashAlgorithm hashAlgorithm)
+    public ConsolePresenter(IDatabaseSelectModel selector)
     {
-        _algorithm = hashAlgorithm;
+        _processor = new ProcessDataModel(selector);
+        _view = new ConsoleView();
     }
     
-    public string Calculate(string input)
+    public void DisplayCitizenInfo()
     {
-        byte[] binaryData = Encoding.UTF8.GetBytes(input);
-        byte[] binaryHash = _algorithm.ComputeHash(binaryData);
+        var passport = new Passport(_view.RequestPassportId());
         
-        return Convert.ToHexString(binaryHash);
-    }
-}
-
-public interface IIdentifier
-{
-    public string Id { get; }
-    public string DisplayId { get; }
-}
-
-public class UserIdentifier : IIdentifier
-{
-    public UserIdentifier(string identifier, IIdentifierValidator validator)
-    {
-        validator.Validate(identifier);
+        if (_processor.TryGetCitizen(passport, out Citizen? citizen) == false)
+        {
+            _view.DisplayMessage($"Данные по паспорту \"{passport.Id}\" не найдены");
+            return;
+        }
         
-        Id = validator.Id;
-        DisplayId = validator.DisplayId;
-    }
-    
-    public string Id { get; }
-    public string DisplayId { get; }
-}
-
-public interface IIdentifierValidator : IIdentifier
-{
-    public void Validate(string input);
-}
-
-public class UserIdentifierValidator : IIdentifierValidator
-{
-    private const int MinIdLength = 10;
-    private const string WhiteSpace = " ";
-    private const string EmptyString = "";
-
-    private string _id = "";
-    private string _displayId = "";
-    
-    public void Validate(string input)
-    {
-        _displayId = input.Trim();
+        string accessResult = citizen!.HasVoteAccess ? "ПРЕДОСТАВЛЕН" : "НЕ ПРЕДОСТАВЛЯЛСЯ";
         
-        if (_displayId == EmptyString)
-            throw new ArgumentException("Identifier is empty");
+        _view.DisplayMessage($"По паспорту \"{citizen.Passport.Id}\" доступ к бюллетеню на дистанционном электронном голосовании {accessResult}");
+    }
+}
 
-        _id = _displayId.Replace(WhiteSpace, EmptyString);
+public interface IDatabaseSelectModel
+{
+    public DataTable SelectPassportData(Passport passport);
+}
+
+public class SqliteSelectModel : IDatabaseSelectModel
+{
+    private SqliteConnection _connection;
+    
+    public SqliteSelectModel(string databaseFilename)
+    {
+        _connection = new SqliteConnection($"Data Source={databaseFilename}");
+    }
+    
+    public DataTable SelectPassportData(Passport passport)
+    {
+        DataTable table = new DataTable();
         
-        if (_id.Length < MinIdLength)
-            throw new ArgumentException($"Identifier must contain at least {MinIdLength} characters");
+        // _connection.Open();
+        // Где-то тут делаем выборку, но в этой библиотеке апи другой, поэтому вот вам смайлик вместо исключения - :)
+        // _connection.Close();
+
+        return table;
+    }
+}
+
+public class ProcessDataModel
+{
+    private IDatabaseSelectModel _selector;
+    
+    public ProcessDataModel(IDatabaseSelectModel selector)
+    {
+        _selector = selector;
     }
     
-    public string Id => _id;
-    public string DisplayId => _displayId;
-}
-
-public interface IDataExtractor
-{
-    public string Data { get; }
-}
-
-public class UserIdentifierExtractor : IDataExtractor
-{
-    private readonly IIdentifier _identifier;
-    
-    public UserIdentifierExtractor(IIdentifier identifier)
+    public bool TryGetCitizen(Passport passport, out Citizen? citizen)
     {
-        _identifier = identifier;
-    }
-    
-    public string Data => _identifier.Id;
-}
-
-public interface IIdentifierGenerator
-{
-    public IIdentifier Create(string id, IIdentifierValidator validator);
-}
-
-public class UserIdentifierGenerator : IIdentifierGenerator
-{
-    public IIdentifier Create(string id, IIdentifierValidator validator)
-    {
-        return new UserIdentifier(id, validator);
+        citizen = null;
+        
+        DataTable data = _selector.SelectPassportData(passport);
+        
+        if (data.Rows.Count == 0)
+            return false;
+        
+        citizen = new Citizen(passport, Convert.ToBoolean(data.Rows[0].ItemArray[1]));
+        return true;
     }
 }
